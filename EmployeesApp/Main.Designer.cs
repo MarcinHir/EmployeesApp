11227441c4df2d078f2913994aa3720c05f09c87
MarcinHir <hirek87@gmail.com>
2023-02-14 21:17:17 +0100

    Add project files.

---
+++ EmployeesApp/Main.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace EmployeesApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddEmployee = new System.Windows.Forms.Button();
+            this.btnEditEmployee = new System.Windows.Forms.Button();
+            this.btnFireEmployee = new System.Windows.Forms.Button();
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbWorking = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddEmployee
+            // 
+            this.btnAddEmployee.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnAddEmployee.Location = new System.Drawing.Point(13, 13);
+            this.btnAddEmployee.Name = "btnAddEmployee";
+            this.btnAddEmployee.Size = new System.Drawing.Size(131, 39);
+            this.btnAddEmployee.TabIndex = 0;
+            this.btnAddEmployee.Text = "Dodaj Pracownika";
+            this.btnAddEmployee.UseVisualStyleBackColor = false;
+            // 
+            // btnEditEmployee
+            // 
+            this.btnEditEmployee.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnEditEmployee.Location = new System.Drawing.Point(13, 58);
+            this.btnEditEmployee.Name = "btnEditEmployee";
+            this.btnEditEmployee.Size = new System.Drawing.Size(131, 39);
+            this.btnEditEmployee.TabIndex = 1;
+            this.btnEditEmployee.Text = "Edytuj Dane";
+            this.btnEditEmployee.UseVisualStyleBackColor = false;
+            // 
+            // btnFireEmployee
+            // 
+            this.btnFireEmployee.BackColor = System.Drawing.Color.Red;
+            this.btnFireEmployee.Location = new System.Drawing.Point(13, 103);
+            this.btnFireEmployee.Name = "btnFireEmployee";
+            this.btnFireEmployee.Size = new System.Drawing.Size(131, 39);
+            this.btnFireEmployee.TabIndex = 2;
+            this.btnFireEmployee.Text = "Zwolnij Pracownika";
+            this.btnFireEmployee.UseVisualStyleBackColor = false;
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(150, 13);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.RowHeadersWidth = 51;
+            this.dgvEmployees.RowTemplate.Height = 24;
+            this.dgvEmployees.Size = new System.Drawing.Size(638, 425);
+            this.dgvEmployees.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 174);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(131, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // lbWorking
+            // 
+            this.lbWorking.AutoSize = true;
+            this.lbWorking.Location = new System.Drawing.Point(31, 155);
+            this.lbWorking.Name = "lbWorking";
+            this.lbWorking.Size = new System.Drawing.Size(97, 16);
+            this.lbWorking.TabIndex = 6;
+            this.lbWorking.Text = "Wybierz z listy :";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbWorking);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dgvEmployees);
+            this.Controls.Add(this.btnFireEmployee);
+            this.Controls.Add(this.btnEditEmployee);
+            this.Controls.Add(this.btnAddEmployee);
+            this.Name = "Main";
+            this.Text = "System Kadrowy";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddEmployee;
+        private System.Windows.Forms.Button btnEditEmployee;
+        private System.Windows.Forms.Button btnFireEmployee;
+        private System.Windows.Forms.DataGridView dgvEmployees;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbWorking;
+    }
+}
+
